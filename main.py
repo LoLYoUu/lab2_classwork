33734d7170fb51ed7390cc427b4ff13d5297ed39
LolYoUu <evgeniy.egorov.99@mail.ru>
2022-04-21 23:13:51 +0300

    погода сегодня

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
-city = "Moscow,RU"
+s_city = "Moscow,RU"
 appid = "ccbcced32f61229d3cd1ae641af0afbc"
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
         params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
